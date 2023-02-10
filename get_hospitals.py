ce53219e2801f2e5471d38701d4acac4e8d99a82
MahamAbbasi07 <52717511+MahamAbbasi07@users.noreply.github.com>
2023-02-11 00:21:07 +0500

    Add functionality in get_hospitals.py

--- get_hospitals.py
+++ get_hospitals.py
@@ -3,6 +3,11 @@ from flask_restful import Api, Resource
 from threading import Thread
 from queue import Queue
 import pandas as pd
+import geopy.distance as geo
+import json
+import os
+
+
 
 class Hospitals(Resource):
     def get(self):
@@ -10,18 +15,18 @@ class Hospitals(Resource):
         lng = request.args.get("lng")
         within = request.args.get("within")
         if lat is None:
-            return make_response(jsonify({'error': 'lat is required as a query parameter'}), 400)
+            return make_response(jsonify({'error': 'Bad request\nlat is required as a query parameter'}), 400)
         elif lng is None:
-            return make_response(jsonify({'error': 'lng is required as a query parameter'}), 400)
+            return make_response(jsonify({'error': 'Bad request\nlng is required as a query parameter'}), 400)
         elif within is None:
-            return make_response(jsonify({'error': 'within is required as a query parameter'}), 400)
+            return make_response(jsonify({'error': 'Bad request\nwithin is required as a query parameter'}), 400)
 
         else:
             # Create a queue to communicate the results from the worker thread
             queue = Queue()
 
             # Start a new thread to do some heavy processing
-            thread = Thread(target=gethospitals, args=(lat,lng,within, queue))
+            thread = Thread(target=self.get_nearby_hospitals, args=((lat,lng),within, queue))
             thread.start()
 
             thread.join()
@@ -32,22 +37,43 @@ class Hospitals(Resource):
             # Return the response
             # json_results = marker_locations
             if marker_locations is not None:
-                return make_response(jsonify(marker_locations), 200)
+                return make_response(jsonify({"response" : marker_locations}), 200)
             else:
                 return make_response(jsonify({'status': 'Bad Request'}), 400)
             
         # else:
         #     return make_response(jsonify({'status': 'Bad Request'}), 400)
 
-def gethospitals(lat,lng,within, queue):
-    # Do some heavy processing here
-    # image_results = {'width': image.width, 'height': image.height}
-    # print(image_results,"\n\n\n")
-    # Put the results in the queue
-    df = pd.read_csv('hospitals.csv')
-    print(df.to_dict(orient='records'))
-    queue.put(df.to_dict(orient='records'))
+    def get_nearby_hospitals(self,current_location, within_distance,queue):
+        """
+        This function takes in a `current_location` in the form of a tuple of latitude and longitude coordinates and a `within_distance` in kilometers,
+        and returns a list of hospitals within that distance from the `current_location`.
+        The result is returned as a JSON string, with each hospital represented as a dictionary.
+        """
+        #Get path of current directory
 
-    # # Write JSON results to a file
-    # with open('image_results.json', 'w') as outfile:
-    #     json.dump(json_results, outfile)
+        path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))).replace("\\","/")+ "/Hospitals.csv"
+
+        # Read the csv file containing information about hospitals into a Pandas dataframe
+        
+        print(path)
+        data = pd.read_csv(path)
+        print(data)
+        # Extract the latitude and longitude values for each hospital from the dataframe
+        latitudes = list(data["Latitude"])
+        longitudes = list(data["Longitude"])
+
+        # Initialize variables to keep track of the hospitals
+        row_index = 0
+        marker_locations = []
+
+        # Loop through the latitudes and longitudes, and calculate the distance between the current location and each hospital
+        for i, j in zip(latitudes, longitudes):
+            if geo.geodesic(current_location, (i, j)).km <= float(within_distance):
+                # If the hospital is within the specified distance, add it to the list of nearby hospitals
+                marker_locations.append(data.iloc[row_index].to_dict())
+            row_index = row_index + 1
+
+        # Return the list of nearby hospitals as a JSON string
+        queue.put(marker_locations)
+        # return json.dumps({"response" : marker_locations}, indent=4)
